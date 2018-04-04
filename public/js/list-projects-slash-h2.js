7fed6efd15ecfb8dd60b403ebc0b314b75b62631
ThibaudLafont <you@example.com>
2018-04-04 14:58:54 +0200

    Implement slash decoration on project-lis titles

---
+++ public/js/list-projects-slash-h2.js
@@ -0,0 +1,21 @@
+function adaptSlashTitle(parent) {
+    // Loop on every node found
+    $(parent).each(function(){
+        // Store h2 height
+        var height = $(this).children('h2').outerHeight();
+        // Set h2 height to slash
+        $(this).children('img').height(height);
+        // Now check if slash appear break title line
+        if(height !== $(this).children('h2').outerHeight()){
+            // Set new height
+            var height = $(this).children('h2').outerHeight();
+            // Set h2 height to slash
+            $(this).children('img').height(height);
+        }
+        // Show slash
+        $(this).children('img').css('opacity', 1);
+    });
+}
+
+adaptSlashTitle('.list-left-column');
+adaptSlashTitle('.category-title');
