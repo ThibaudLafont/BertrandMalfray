16226014e48488ee4009f383c9c3d510b9cd5aaf
ThibaudLafont <you@example.com>
2018-04-05 14:59:40 +0200

    Build slashes

---
+++ public/js/show-slash-config.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    adaptSlashTitle('.project-title', 'h1');
+});
+$(window).resize(function(){
+    adaptSlashTitle('.project-title', 'h1');
+})
--- public/js/slash-ajust.js
+++ public/js/slash-ajust.js
@@ -1,19 +1,19 @@
-function adaptSlashTitle(parent) {
+function adaptSlashTitle(parent, children) {
     // Loop on every node found
     $(parent).each(function(){
         // Store h2 height
-        var height = $(this).children('h2').outerHeight();
+        var height = $(this).children(children).outerHeight();
         // Set h2 height to slash
-        $(this).children('img').height(height);
+        $(this).children('.slash').height(height);
         // Now check if slash appear break title line
-        if(height !== $(this).children('h2').outerHeight()){
+        if(height !== $(this).children(children).outerHeight()){
             // Set new height
-            var height = $(this).children('h2').outerHeight();
+            var height = $(this).children(children).outerHeight();
             // Set h2 height to slash
-            $(this).children('img').height(height);
+            $(this).children('.slash').height(height);
         }
         // Show slash
-        $(this).children('img').css('opacity', 1);
+        $(this).children('.slash').css('opacity', 1);
     });
 }
 
