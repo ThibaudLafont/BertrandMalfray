6b7ec94fdbc7c47899d26b361c23608637f521be
ThibaudLafont <you@example.com>
2018-04-05 13:38:33 +0200

    Lite fix of galery'

--- public/js/show-project-gallery.js
+++ public/js/show-project-gallery.js
@@ -3,8 +3,6 @@ $('.slick-container').slick({
     arrow: true,
     dots: true,
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
     centerMode: true,
     variableWidth: true
 });
