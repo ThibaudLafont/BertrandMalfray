821c800e030f960116a083249c76fe84bd0db697
ThibaudLafont <you@example.com>
2018-04-13 19:28:40 +0200

    Front ajustments

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,6 +6,7 @@ use App\Entity\Project\Lists\CategoryList;
 use App\Entity\Project\Lists\ProjectList;
 use App\Entity\Project\Project;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends Controller
@@ -28,12 +29,31 @@ class CategoryController extends Controller
                 ['slugName' => $cat]
             );
 
+        if(is_null($cat)) {
+//            throw new NotFoundHttpException("Aucune resource ici");
+            return $this->redirectToRoute('no_project');
+        }
+
         $projects = $cat->getProjects();
 
         return $this->render(
             'project/category.html.twig',
             ['projects' => $projects]
         );
+
+    }
+
+    /**
+     * @Route(
+     *     "/category/empty",
+     *     name="no_project"
+     * )
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function noProjectAction() {
+
+        return $this->render('project/no-project.html.twig');
 
     }
 
